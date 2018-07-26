1b0499491ecd43c83db6cc05de8de264bbf70556
shorinbonsai <shorinbonsai@gmail.com>
2018-07-26 14:58:33 -0400

    Splash screen with music and level load

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        Invoke("LoadNextLevel", levelLoadDelay);
+
+
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
