078f99dcdb67b1d7813c6d28127ba1e2d4d6759e
shorinbonsai <shorinbonsai@gmail.com>
2018-08-21 16:34:57 -0400

    testing collision and triggers / baked lighting

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+
+    void Start()
+    {
+
+        Invoke("LoadNextLevel", levelLoadDelay);
+
+
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
